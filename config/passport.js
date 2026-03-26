166ee830291efdd282c7f62d1e67b524590163d7
Roshni-glitch <roshnidev2755@gmail.com>
2026-03-26 12:46:53 +0530

    first commit

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,8 @@ passport.use(
       {
         clientID: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET,
-        callbackURL: "https://task-kero.onrender.com/auth/google/callback",
+        //callbackURL: "https://task-kero.onrender.com/auth/google/callback",
+        callbackURL: "http://localhost:3000/auth/google/callback",
         scope: ["profile", "email"],
       },
       async (accessToken, refreshToken, profile, done) => {
